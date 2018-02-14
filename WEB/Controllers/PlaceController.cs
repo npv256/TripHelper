b2556c056ea480be59023b55308cd6b21d4aec2c
Павел Носков <npv256@gmail.com>
2018-02-14 10:24:43 +0700

    PlaceController

--- WEB/Controllers/PlaceController.cs
+++ WEB/Controllers/PlaceController.cs
@@ -6,6 +6,7 @@ using System.Web.Mvc;
 using AutoMapper;
 using BLL.Interfaces;
 using DLL.Entities;
+using Microsoft.Ajax.Utilities;
 using WEB.Models;
 
 namespace WEB.Controllers
@@ -25,7 +26,7 @@ namespace WEB.Controllers
         {
             var config = new MapperConfiguration(cfg => cfg.CreateMap<Place, PlaceViewModels>());
             var mapper = config.CreateMapper();
-            var placeList = mapper.Map<IEnumerable<Place>, IEnumerable<PlaceViewModels>>(_placeService.GetItemList());
+            var placeList = mapper.Map<List<Place>, List<PlaceViewModels>>(_placeService.GetItemList().ToList());
             return View(placeList);
         }
 
@@ -46,62 +47,80 @@ namespace WEB.Controllers
 
         // POST: Place/Create
         [HttpPost]
-        public ActionResult Create(FormCollection collection)
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(PlaceViewModels plVModel, IEnumerable<HttpPostedFileBase> fileData)
         {
             try
             {
-                // TODO: Add insert logic here
+                if (ModelState.IsValid)
+                {
+                    if (_placeService.GetItemList().FirstOrDefault(place => place.Name == plVModel.Name) == null)
+                    {
+                        var config = new MapperConfiguration(cfg => cfg.CreateMap<PlaceViewModels, Place>());
+                        var mapper = config.CreateMapper();
+                        var placeModel = mapper.Map<PlaceViewModels, Place>(plVModel);
 
-                return RedirectToAction("Index");
+                        if (fileData != null)
+                        {
+                            foreach (var file in fileData)
+                            {
+                                    string fileName = System.IO.Path.GetFileName(file.FileName);
+                                    file.SaveAs(Server.MapPath("../Content/Images/" + fileName));
+                            }
+                            placeModel.Pictures = fileData.Select(x => "../../Content/Images/" + System.IO.Path.GetFileName(x.FileName)).ToArray();
+                        }
+
+                        _placeService.Create(placeModel);
+                        _placeService.Save();
+                        return RedirectToAction("Index");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("", "Place with such name is exist");
+                    }
+                }
             }
             catch
             {
-                return View();
+                return View(plVModel);
             }
+
+            return View(plVModel);
         }
 
         // GET: Place/Edit/5
-        public ActionResult Edit(int id)
+        public ActionResult Edit(long id)
         {
-            return View();
+            var config = new MapperConfiguration(cfg => cfg.CreateMap<Place, PlaceViewModels>());
+            var mapper = config.CreateMapper();
+            var plVModel = mapper.Map<Place, PlaceViewModels>(_placeService.GetItem(id));
+            return View(plVModel);
         }
 
         // POST: Place/Edit/5
         [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
+        public ActionResult Edit(long id, PlaceViewModels plVModels, IEnumerable<HttpPostedFileBase> fileData)
         {
             try
             {
-                // TODO: Add update logic here
-
+                var config = new MapperConfiguration(cfg => cfg.CreateMap<PlaceViewModels, Place>());
+                var mapper = config.CreateMapper();
+               var placeModel = mapper.Map<PlaceViewModels, Place>(plVModels);
+                _placeService.Update(placeModel);
+                _placeService.Save();
                 return RedirectToAction("Index");
             }
-            catch
+            catch(Exception e)
             {
-                return View();
+                return View(plVModels);
             }
         }
 
-        // GET: Place/Delete/5
-        public ActionResult Delete(int id)
+        public ActionResult Delete(long id)
         {
-            return View();
-        }
-
-        // POST: Place/Delete/5
-        [HttpPost]
-        public ActionResult Delete(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add delete logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
+            _placeService.Delete(id);
+            _placeService.Save();
+            return RedirectToAction("Index");
         }
     }
 }
